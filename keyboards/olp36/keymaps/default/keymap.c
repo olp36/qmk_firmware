cb962acca7f06e9a42572ef05f1ac190a0f9033b
olp36 <65201864+olp36@users.noreply.github.com>
2022-12-22 14:31:51 +0900

    fix windows keycodes

--- keyboards/olp36/keymaps/default/keymap.c
+++ keyboards/olp36/keymaps/default/keymap.c
@@ -12,35 +12,47 @@ enum layers {
 #define MO_SYS  MO(KL_SYS)
 
 /* MOD */
-#define MD_LALT LALT_T(KC_LNG2) // Left Alt when held, muhenkan when tapped
+#define MD_LALT LALT_T(KC_INT5) // Left Alt when held, muhenkan when tapped
 #define MD_LCTL LCTL_T(KC_BSPC) // Left Control when held, BSPC when tapped
 #define MD_LSFT LSFT_T(KC_SPC) // Left Shift when held, SPC when tapped
 
 /* JPN KEY */
-#define JP_HENK KC_LNG1 // JIS henkan
 #define JP_LMKK KC_ASTR // JIS (
 #define JP_RMKK KC_LPRN // JIS )
-#define JP_AND  LSFT(KC_6)  // JIS &
 #define JP_CLN  KC_QUOT // JIS :
 #define JP_AT   KC_LBRC // JIS @
 #define JP_CIRC KC_EQL  // JIS ^
-#define JP_PERC LSFT(KC_5)  // JIS %
-#define JP_SQUO LSFT(KC_7)  // JIS '
-#define JP_DQUO LSFT(KC_2)  // JIS "
-#define JP_DOLL LSFT(KC_4)  // JIS $
-#define JP_SHAP LSFT(KC_3)  // #
-#define JP_VBAR LSFT(KC_INT3)  // |
 #define JP_LKKO KC_RBRC  // JIS [
 #define JP_RKKO KC_NUHS  // JIS ]
-#define JP_UNDR LSFT(KC_INT1) // JIS _
-#define JP_BSLS LOPT(KC_INT3) // JIS Back Slash
+#define JP_DQUO LSFT(KC_2)  // JIS "
+#define JP_SHAP LSFT(KC_3)  // JIS #
+#define JP_DOLL LSFT(KC_4)  // JIS $
+#define JP_PERC LSFT(KC_5)  // JIS %
+#define JP_AND  LSFT(KC_6)  // JIS &
+#define JP_SQUO LSFT(KC_7)  // JIS '
 #define JP_EQL  LSFT(KC_MINS) // JIS =
 #define JP_PLUS LSFT(KC_SCLN) // JIS +
 #define JP_ASTR LSFT(KC_QUOT) // JIS *
-#define JP_JYEN KC_INT1 // JIS YEN
+#define JP_BSLS KC_INT1 // JIS Back Slash
+#define JP_UNDR LSFT(KC_INT1) // JIS _
+#define JP_JYEN KC_INT3 // JIS YEN
+#define JP_VBAR LSFT(KC_INT3)  // JIS |
+#define JP_HENK KC_INT4 // JIS henkan
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+  /* MAIN
+* ,----------------------------------------------------------------------.
+* |   Q  |   W  |   E  |   R  |   T  ||   Y  |   U  |   I  |   O  |   P  |
+* |------+------+------+------+------++------+------+------+------+------|
+* |   A  |   S  |   D  |   F  |   G  ||   H  |   J  |   K  |   L  |  ENT |
+* |------+------+------+------+------++------+------+------+------+------|
+* |   Z  |   X  |   C  |   V  |   B  ||   N  |   M  |   ,  |   .  |   /  |
+* `------+------+------+------+------++------+------+------+------+------'
+*               |muhenk|      |  BS  ||  SPC |      |      |
+*               |  Alt |NumLyr|Contrl|| Shift|FunLyr|henkan|
+*               `------------------------------------------'
+*/
     [KL_MAIN] = LAYOUT_ortho_4x10(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
@@ -48,6 +60,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           MD_LALT, MO_NUM,  MD_LCTL, MD_LSFT, MO_FUN,  JP_HENK
     ),
 
+    /* FUNCTION
+ * ,----------------------------------------------------------------------.
+ * |  ESC |   '  |  up  |   "  |   $  ||   ;  |   :  |   (  |   )  |   @  |
+ * |------+------+------+------+------++------+------+------+------+------|
+ * |  TAB | left |  down |right|   #  ||  Yen |   ^  |   &  |   %  |   -  |
+ * |------+------+------+------+------++------+------+------+------+------|
+ * |   |  |VolDwn| MUTE | VolUp|BSlash||   ~  |   _  |   [  |   ]  |   !  |
+ * `------+------+------+------+------++------+------+------+------+------'
+ *               |muhenk|      |      ||  SPC |      |      |
+ *               |  Alt |SysLyr|  DEL || Shift|      |  WIN |
+ *               `------------------------------------------'
+ */
     [KL_FUN] = LAYOUT_ortho_4x10(
         KC_ESC,  JP_SQUO, KC_UP,   JP_DQUO, JP_DOLL, KC_SCLN, JP_CLN,  JP_LMKK, JP_RMKK, JP_AT,
         KC_TAB,  KC_LEFT, KC_DOWN, KC_RGHT, JP_SHAP, JP_JYEN, JP_CIRC, JP_AND,  JP_PERC, KC_MINS,
@@ -55,6 +79,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          _______, MO_SYS,   KC_DEL,  _______, XXXXXXX, KC_LWIN
     ),
 
+    /* NUMBER
+ * ,----------------------------------------------------------------------.
+ * |      |  F10 |  F7  |  F8  |  F9  ||   7  |   8  |   9  |   0  |   =  |
+ * |------+------+------+------+------++------+------+------+------+------|
+ * |      |  F11 |  F4  |  F5  |  F6  ||   4  |   5  |   6  |   +  |   -  |
+ * |------+------+------+------+------++------+------+------+------+------|
+ * |      |  F12 |  F1  |  F2  |  F3  ||   1  |   2  |   3  |   *  |   /  |
+ * `------+------+------+------+------++------+------+------+------+------'
+ *               |muhenk|      |      ||      |      |      |
+ *               |  Alt |      |  DEL ||   ,  |   0  |   .  |
+ *               `------------------------------------------'
+ */
     [KL_NUM] = LAYOUT_ortho_4x10(
         XXXXXXX, KC_F10,  KC_F7,   KC_F8,   KC_F9,   KC_7,    KC_8,    KC_9,    KC_0,    JP_EQL,
         XXXXXXX, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    JP_PLUS, KC_MINS,
@@ -62,6 +98,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______, XXXXXXX, KC_DEL,  KC_COMM, KC_0,    KC_DOT
     ),
 
+    /* SYSTEM
+ * ,----------------------------------------------------------------------.
+ * | RESET|      |      |      |      ||      |      |      |      | SLEEP|
+ * |------+------+------+------+------++------+------+------+------+------|
+ * |      |      |      |      |      ||      |      |      |      |      |
+ * |------+------+------+------+------++------+------+------+------+------|
+ * |      |      |      |      |      ||      |      |      |      | POWER|
+ * `------+------+------+------+------++------+------+------+------+------'
+ *               |      |      |      ||      |      |      |
+ *               |      |      |      ||      |      |      |
+ *               `------------------------------------------'
+ */
     [KL_SYS] = LAYOUT_ortho_4x10(
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLEP,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
